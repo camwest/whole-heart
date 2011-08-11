7e6b3d84e5171c06ced2e4cd4e248fe84aacbe5e
Cameron Westland <cameron@bigbangtechnology.com>
2011-08-10 20:06:20 -0400

    Refactored into a simple static html app

---
+++ web.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express.createServer(express.logger());
+
+app.get('/', function(req, res) {
+  res.render('index.ejs');
+});
+
+var port = process.env.PORT || 3000;
+
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
+
+
+app.configure('development', function() {
+  app.use(express.static(__dirname + '/public'));
+});
